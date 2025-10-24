0e3817d07ce9f55688f67b53f51e9560799e1741
Mohammed-imran243 <mohemmedimran433k@gmail.com>
2025-10-24 22:46:04 +0530

    first commit

---
+++ SunString.java
@@ -0,0 +1,13 @@
+ class SunString{
+    public static void main(String[] args) {
+         String str = "abab";
+         String SubString  = "ab";
+
+        if(str == SubString)
+        {
+            System.out.println("True"); 
+        }else{
+            System.out.println("False");;
+        }
+    }
+ }
